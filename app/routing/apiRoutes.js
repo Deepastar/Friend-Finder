67759f88983c90b5853b272e9480f03e400847ce
Deepa Ravichandran <deepa5starbee@gmail.com>
2018-08-23 01:13:53 -0400

    worked on seperate folders

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,36 @@
+var friendsList=require("../app/data/friends");
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsList);
+      });
+
+      app.post("/api/friends", function(req, res) {
+        var user1 = req.body;
+        var scoreDifference = [];
+        if (friendsList.length >1) {
+            friendsList.forEach(user1);{
+                var totalDiff=0;
+                for(var i=0; i<user1.answers.length; i++){
+                    var thisAns = user.answers[i]; 
+                    var otherAns = user1.answers[i];
+                    var scoreDiff = +otherAns - +thisAns;
+                    totalDiff += Math.abs(scoreDiff);
+                }
+                scoreDifference.push(totalDiff);
+            }
+
+            var minDiff = math.min.apply(null, scoreDifference);
+            var bestMatch = [];
+            for(var i=0; i<scoreDifference.length; i++) {
+                if (scoreDifference[i] === minDiff){
+                    bestMatch.push(friendsList[i]);
+                }
+            }
+            res.json(bestMatch);
+        }
+        else{
+            res.json(friendsList);
+        }
+        friendsList.push(user1);
+    });
+};
---
+++ app/server.js
@@ -0,0 +1,21 @@
+// npm packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+//Creating express server with a var nae calles app
+var app = express();
+//Setting initial Port
+var PORT = process.env.PORT || 8080;
+//Setting exp to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//Type for various different custom JSON types as JSON
+app.use(bodyParser.json({type:'application/*+json'}))
+//Routing here
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+// Listener to start server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
